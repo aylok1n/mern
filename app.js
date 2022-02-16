7c28e9f9c625dc9ebef1119ca66aaa9e7eef1a75
aylok1n <nikolya.blog@gmail.com>
2022-02-16 19:41:30 +0400

    add initial client

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require('express')
+require('node-env-file')('.env')
+
+
 const app = express()
 
 
+const { PORT } = process.env
+
+app.get('/', (req, res) => {
+    res.send('123123')
+})
+
+app.listen(PORT, () => `Server started at ${PORT} port`)
---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+
+export default App;
