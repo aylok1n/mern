3f83269ea692327780b9e46f2acef7d93c694fc9
Tim-ru <httimoha176@gmail.com>
2022-03-05 16:23:20 +0400

    Fix chat styles

--- client/src/routes/dialogs.tsx
+++ client/src/routes/dialogs.tsx
@@ -4,6 +4,7 @@ import { useContext, useState } from "react";
 import { ChatContext } from "../context/ChatContext";
 import { Outlet } from "react-router-dom";
 import { Menu } from "@mui/icons-material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 export const ChatsPage = () => {
     const { chats, chatWith } = useContext(ChatContext)
@@ -17,7 +18,7 @@ export const ChatsPage = () => {
     return (
         <div className="w-full bg-white rounded h-full max-w-screen-xl flex flex-row">
             <List
-                className="scroll flex h-full bg-stone-100"
+                className="scroll flex h-full bg-white border-r-4 border-gray-100 z-50"
                 sx={{
                     display: {
                         xs: chatWith ? 'none' : 'block',
@@ -27,24 +28,36 @@ export const ChatsPage = () => {
                         xs: '100vw',
                         lg: '30%'
                     },
-                    borderRight: {
-                        xs: '',
-                        md: '1px'
-                    }
                 }}
             >
                 {chats.map((chat, index) => <ChatItem key={index} chat={chat} />)}
             </List>
             <div className="w-full relative">
-                {chatWith?.name && <div className={'border-b bg-white w-full h-16 border-gray-300 flex absolute items-center justify-center'}>
-                    <IconButton
-                        onClick={toggleDrawer}
-                        sx={{ display: { xs: 'block', lg: 'none' }, position: 'absolute', left: 10 }}
-                    >
-                        {!!chatWith?.name && <Menu />}
-                    </IconButton>
-                    <h3>{chatWith?.name}</h3>
-                </div>}
+                {chatWith?.name &&
+                    <div className={'border-b-4 bg-white w-full border-gray-100 flex absolute items-center justify-start py-2 px-5'}>
+                        <IconButton
+                            onClick={toggleDrawer}
+                            sx={{ display: { xs: 'block', lg: 'none' }, position: 'absolute', left: 10 }}
+                        >
+                            {!!chatWith?.name && <Menu />}
+                        </IconButton>
+                        <div className="w-12 h-12">
+                            <AccountCircle sx={{ width: '100%', height: '100%' }} />
+                        </div>
+                        <div className="flex flex-col ml-2">
+                            <div className="flex flex-row items-center">
+                                <span className="text-black mr-20 font-semibold">{chatWith?.name}</span>
+                            </div>
+                            <div className="flex flex-row items-center">
+                                <span className="text-sm text-gray-400 truncate">
+                                    Offline
+                                </span>
+                                <span className="text-sm text-gray-400 truncate ml-4">
+                                    Last seen 2 days ago
+                                </span>
+                            </div>
+                        </div>
+                    </div>}
                 <Drawer
                     sx={{ display: { xs: 'block', lg: 'none' } }}
                     role="presentation"
@@ -53,13 +66,14 @@ export const ChatsPage = () => {
                 >
                     {chats.map((chat, index) => <ChatItem key={index} chat={chat} />)}
                 </Drawer >
-                <Container sx={{
+                <Container disableGutters sx={{
                     display:
                     {
                         xs: chatWith ? 'flex' : 'none',
                         lg: 'flex'
                     },
-                    maxHeight: '100%'
+                    maxHeight: '100%', 
+                    padding: 0
                 }}>
                     <Outlet />
                 </Container>
