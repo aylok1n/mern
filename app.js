767ee369b41246fc1b1edbece14ac48951bf1a7b
aylok1n <nikolya.blog@gmail.com>
2022-03-14 12:08:35 +0400

    disable cors

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const path = require('path')
 
 require('node-env-file')('.env')
 const { PORT, PRODUCTION, MONGOURI } = process.env
 
 const app = express()
-const cors = require('cors')
+
 app.use(express.json({ extended: true }))
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Content-Type", 'application/json');
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/search', require('./routes/search.routes'))
 app.use('/api/chat', require('./routes/chat.routes'))
-app.use(cors())
+
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000", "*", "*:*"],
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
